f1a49f046ee9c591b5072092f35c24a43d19f525
畠中晃弘 <hatanaka@questetra.com>
2020-11-27 22:55:52 +0900

    aws access_key secret_key の設定追加

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "access_key" {
+  type        = string
+  description = "AWS Access Key."
+}
+variable "secret_key" {
+  type        = string
+  description = "AWS Secret Key."
+}
+
 ####################
 #        AWS       #
 ####################
