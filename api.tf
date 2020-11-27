7b7218267777e90be36f5240e740d286c81c9136
畠中晃弘 <hatanaka@questetra.com>
2020-11-27 23:12:48 +0900

    Terraform Resource の設定修正

--- api.tf
+++ api.tf
@@ -64,7 +64,7 @@ resource "aws_api_gateway_rest_api" "myLambdaFunction-API" {
   }
 }
 
-resource "aws_api_gateway_resource" "myKintoneFunction-API-Resource" {
+resource "aws_api_gateway_resource" "myLambdaFunction-API-Resource" {
   rest_api_id = aws_api_gateway_rest_api.myLambdaFunction-API.id
   parent_id   = aws_api_gateway_rest_api.myLambdaFunction-API.root_resource_id
   path_part   = var.api_path
